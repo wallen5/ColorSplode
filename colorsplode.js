9128230089937768a713cfc7a573e53863938473
Dylan Strnad <dylanstrnad@outlook.com>
2025-09-09 17:09:41 -0400

    Home Screen
    added start button, and color fluctuation for game title

--- colorsplode.js
+++ colorsplode.js
@@ -2,20 +2,71 @@ var player;
 var character;
 var playerX = 375;
 var playerY = 375;
+let time = 0;
+let r = 250;
+let g = 0;
+let b = 0;
+
+let state = 0;
+let startButton;
 
 
 function preload(){
   character = loadImage("images/redpaintbucketgif.gif");
+  myFont = loadFont('font/PressStart2P-Regular.ttf');
 }
 
 function setup() {
+
+  
   createCanvas(800, 800);
   character.resize(50, 50);
+
+  //start button
+  textFont(myFont);
+  startButton = new Sprite(400, 450);
+  startButton.text = "Play Game";
+  startButton.width = 120;
+  startButton.height = 50;
+  startButton.color = "lightgreen";
+
 }
 
 function draw() {
-  background(220);
-  playerMovement();
+  if(state == 0){ //start screen
+    background("grey");
+
+    colorFluctuation();
+
+    fill(r, g, b);
+    stroke("black");
+    strokeWeight(5);
+    textSize(30);
+    textStyle("bold");
+    
+    text("ColorSplode", 250 , 350 );
+
+    if (startButton.mouse.pressing()){
+      startButton.remove();
+      state = 1;
+    }
+
+    
+  } else if (state == 1){ //game screen
+      background(220);
+      playerMovement();
+  }
+}
+
+function colorFluctuation(){
+  if(r > 230 || b > 220 || g > 220){
+      r = random(0,100);
+      b = random(0,100);
+      g = random(0,100);
+    }
+    r += random(0,1);
+    g += random(0,3);
+    b += random(0,3);
 }
 
 function keyReleased(){
